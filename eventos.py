79767a101b013ebd1b4983827295d330484b56d6
Alanderson Lima <lima.alanderson@escolar.ifrn.edu.br>
2025-06-29 21:46:20 -0300

    criação da pasta de imagens, adição e correção de erros relacionadios a função "acessar_itens()"

--- eventos.py
+++ eventos.py
@@ -7,7 +7,6 @@ def limpar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def mina(jogador):
-    limpar_terminal()
     print("Cidadão: Rei! eu achei esta pequena quantia de ouro e gostaria de dividir com o reino!")
     print(" (+5 dinheiro)")
     jogador.dinheiro += 5
@@ -19,7 +18,6 @@ def mina(jogador):
     time.sleep(5)
 
 def furto(jogador):
-    limpar_terminal()
     print("Guarda: Rei, identificamos um ladrão, mas não conseguimos impedir completamente o furto")
     print(" (-5 dinheiro)")
     jogador.dinheiro -= 5
@@ -65,57 +63,65 @@ def pandemia(jogador):
             print ("Por favor, digite apenas S ou N")
     
 def demonio(jogador):
-    limpar_terminal()
     print("Demônio: HAHAHA, rei, seus dias de reinado estão contados!")
+    time.sleep(1)
     print("Demônio: Lhe ofereço uma proposta, você pode rolar meu dado, mas números baixos contem coisas negativas, números altos podem te ajudar")
+    time.sleep(1)
     print("Demônio: Iai, temos um acordo ou está tremendo demais? ")
+    print("responda com S/N")
     while True:
         resposta = input().strip().upper()
         if resposta == "S":
             time.sleep(5)
-            print ("Demônio, vejo que você tem muita coragem, não? Vamos ver o que o dado nos diz")
+            print ("Demônio: vejo que você tem muita coragem, não? Vamos ver o que o dado nos diz")
             dado = random.randint(1, 6)
             if dado == 1 or dado == 2:
-                print(f"Ora, um {dado}, você tem bastante azar, HAHAHAHA")
+                print(f"Demônio: Ora, um {dado}, você tem bastante azar, HAHAHAHA")
                 if dado == 1:
                     valor_1 = random.randint(15, 20)
                     valor_2 = random.randint(15, 20)
                     jogador.saude -= valor_1
                     jogador.dinheiro -= valor_2
                     print (f"Você perdeu {valor_1} de saude e {valor_2} de dinheiro")
+                    break
                 else:
                     valor_1 = random.randint(10, 15)
                     valor_2 = random.randint(10, 15)
                     jogador.saude -= valor_1
                     jogador.dinheiro -= valor_2
                     print (f"Você perdeu {valor_1} de saude e {valor_2} de dinheiro")
+                    break
             elif dado == 3 or dado ==4:
-                print (f"Olha o que nós temos, {dado}, sua sorte e seu azar parece em equílibrio, nada mal")
+                print (f"Demônio Olha o que nós temos, {dado}, sua sorte e seu azar parece em equílibrio, nada mal")
                 if dado == 3:
                     valor_1 = random.randint(5, 10)
                     valor_2 = random.randint(5, 10)
                     jogador.saude -= valor_1
                     jogador.dinheiro -= valor_2
                     print (f"Você perdeu {valor_1} de saude e {valor_2} de dinheiro")
+                    break
                 else:
                     valor_1 = random.randint(5, 10)
                     valor_2 = random.randint(5, 10)
                     jogador.saude += valor_1
                     jogador.dinheiro += valor_2
                     print (f"Você ganhou {valor_1} de saude e {valor_2} de dinheiro")
+                    break
             else:
-                print (f"O QUE?? UM {dado}??? Você tem bastante sorte, mas eu voltarei, e isso não se repetirá!")
+                print (f"Demônio: O QUE?? UM {dado}??? Você tem bastante sorte, mas eu voltarei, e isso não se repetirá!")
                 if dado == 5:
                     valor_1 = random.randint(15, 20)
                     valor_2 = random.randint(15, 20)
                     jogador.saude += valor_1
                     jogador.dinheiro += valor_2
                     print (f"Você ganhou {valor_1} de saude e {valor_2} de dinheiro")
+                    break
                 else:
                     print ("Você ganhou um exército de uso único!")
                     print ("Na aba 'itens' você pode usá-lo para saquear seu inimigo, e então você perderá este item")
                     print ("Para usar, basta digitar 'exercito' sem acento")
                     itens_pendentes.append("exercito")
+                    break
                     
         elif resposta == "N":
             time.sleep(5)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import time
-from eventos import random_evento_obg, random_evento_escolha
+from eventos import random_evento_obg, random_evento_escolha, limpar_terminal
 from recursos import itens_pendentes
 CATALOGO_PAISES = {
     "Brasil": {
@@ -14,7 +14,6 @@ CATALOGO_PAISES = {
         "saude": 45
     }
 }
-
 class Jogador:
     def __init__(self, nome, pais):
         ficha = CATALOGO_PAISES[pais]
@@ -34,19 +33,27 @@ class Jogador:
         else:
             print ("O jogador não possui o item a ser removido.")
 
+    def acessar_itens(self):
+        if len(self.itens) == 0:
+            print ("Você não possui itens, faça outra escolha agora")
+        else:
+            for i in range(len(self.itens)):
+                print(f"{i+ 1}. {self.itens[i]}")
+            print ("Deseja usar algum deles? Se sim, digite o nome do item, se não, digite sair")
+            while True:
+                resposta = input().strip().upper()
+                if resposta == "EXERCITO" or resposta == "EXÉRCITO":
+                    self.itens.remove("exercito")
+                if resposta == "SAIR":
+                    break
+                else:
+                    print ("Insira uma resposta válida")
+
 j1 = Jogador("j1", "Brasil")
 j2 = Jogador("j2", "Franca")
 
 rodadas = 1
 vez = 1
-
-def acessar_itens(jogador):
-    if not jogador.itens:
-        print ("Você não possui itens")
-    else:
-        for i in range(len(jogador.itens)):
-            print(jogador.itens[i - 1])
-        print ("Deseja usar algum deles? Se sim, digite o nome do item, se não, digite sair")
 
 
 def rodada(jogador):
@@ -54,21 +61,54 @@ def rodada(jogador):
     random_evento_obg(jogador)
     time.sleep(5)
     random_evento_escolha(jogador)
-    jogador.itens.append(itens_pendentes)
+    jogador.itens.extend(itens_pendentes)
     itens_pendentes.clear()
 
 def turno():
     global vez, rodadas
-    if vez == 1:
+    limpar_terminal()
+    if vez == 1:        
         player_atual = j1
         print (f"Vez de {player_atual.nome}")
         time.sleep(2.5)
         rodada(player_atual)
+        time.sleep(2.5)
+        while True:
+            print (""" Escolha uma das alternativas:
+                1. Itens
+                2. terminar rodada""")
+            escolha = input()
+            if escolha == "1":
+                player_atual.acessar_itens()
+            elif escolha == "2":
+                break
+            else:
+                print ("Digite uma alternativa válida")
+
         vez +=1
     elif vez ==2:
+        player_atual = j2
         print(f"Vez de {player_atual.nome}")
         time.sleep(2.5)
         rodada(player_atual)
+        while True:
+            print (""" Escolha uma das alternativas:
+                1. Itens
+                2. terminar rodada""")
+            escolha = input()
+            if escolha == "1":
+                player_atual.acessar_itens()
+            elif escolha == "2":
+                break
+            else:
+                print ("Digite uma alternativa válida")
+                while True:
+                    escolha = input()
+                    if escolha == "1":
+                        player_atual.acessar_itens()
+                    elif escolha == "2":
+                        break
+            break
         vez -=1
         rodadas += 1
 
